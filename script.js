a7b76152f20e1aa15d6e00dee932c1f86f8ab6d5
Pvk <89601471+PvKriiger@users.noreply.github.com>
2025-05-07 18:20:44 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const botao = document.querySelector("#botao");
+const exibirTexto = document.querySelector("exibirTexto");;
+botao.addEventListener("click", e=>{
+  exibirTexto.textContent = "Bem-vindo(a)!";
+})
