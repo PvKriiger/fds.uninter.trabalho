f78758ff9ba9b293efda846e347fafc40b302491
Pvk <89601471+PvKriiger@users.noreply.github.com>
2025-05-08 15:21:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.querySelector("#botao");
 const exibirTexto = document.querySelector("#exibirTexto");;
 botao.addEventListener("click", e=>{
-  exibirTexto.textContent = ""Busquem conhecimento!" (BILU, ET.)";
+  exibirTexto.textContent = "'Busquem conhecimento!' (BILU, ET.)";
 })
